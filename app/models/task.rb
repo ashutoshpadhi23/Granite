9b1c22d0677a1c5d091c03106cd1b47442ae983a
Ashutosh Padhi <ashutosh.padhi@bigbinary.com>
2025-06-18 11:59:21 +0000

    Added a column in dashboard to display comment counts

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,9 @@
 class Task < ApplicationRecord
   MAXIMUM_TITLE_LENGTH = 125
   VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9].*\z/i
-
+  RESTRICTED_ATTRIBUTES = %i[title task_owner_id assigned_user_id]
+  enum :progress, { pending: "pending", completed: "completed" }, default: :pending
+  enum :status, { unstarred: "unstarred", starred: "starred" }, default: :unstarred
   has_many :comments, dependent: :destroy
   belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
   belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
@@ -13,6 +15,14 @@ class Task < ApplicationRecord
   validate :slug_not_changed
 
   before_create :set_slug
+
+  def self.of_status(progress)
+    if progress == :pending
+      pending.in_order_of(:status, %w(starred unstarred)).order("updated_at DESC")
+    else
+      completed.in_order_of(:status, %w(starred unstarred)).order("updated_at DESC")
+    end
+  end
 
   private
 
