b158ef85f2860250601a2badbc0ebc385e45cd64
ashutoshpadhi23 <ashutosh.padhi@bigbinary.com>
2025-06-21 18:35:14 +0530

    Added starring feature to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,7 @@ class Task < ApplicationRecord
   VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9].*\z/i
   RESTRICTED_ATTRIBUTES = %i[title task_owner_id assigned_user_id]
 
+  enum :status, { unstarred: "unstarred", starred: "starred" }, default: :unstarred
   enum :progress, { pending: "pending", completed: "completed" }, default: :pending
   has_many :comments, dependent: :destroy
   belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
@@ -16,6 +17,14 @@ class Task < ApplicationRecord
   before_create :set_slug
 
   private
+
+    def self.of_status(progress)
+      if progress == :pending
+        pending.in_order_of(:status, %w(starred unstarred)).order("updated_at DESC")
+      else
+        completed.in_order_of(:status, %w(starred unstarred)).order("updated_at DESC")
+      end
+    end
 
     def set_slug
       title_slug = title.parameterize
