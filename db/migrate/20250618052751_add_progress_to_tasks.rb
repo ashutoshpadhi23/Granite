4583e1ef61363fb5a9ebf92e258ac93bfced7970
ashutoshpadhi23 <ashutosh.padhi@bigbinary.com>
2025-06-25 17:38:47 +0530

    Fixed everything

--- db/migrate/20250618052751_add_progress_to_tasks.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddProgressToTasks < ActiveRecord::Migration[7.1]
-  def change
-    add_column :tasks, :progress, :string, default: "pending", null: false
-  end
-end
--- db/migrate/20250618115033_add_comments_count_to_tasks.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddCommentsCountToTasks < ActiveRecord::Migration[7.1]
-  def change
-    add_column :tasks, :comments_count, :integer
-  end
-end
